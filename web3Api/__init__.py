5174dd1759ecbbf90a42130ca00e2607d5a18f60
Murat Saglam <muratusaglam@gmail.com>
2022-10-28 17:49:09 +0100

    Fixed repo structure

---
+++ web3Api/__init__.py
@@ -0,0 +1,10 @@
+# Django data handler
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Web3 API
+from web3 import Web3, HTTPProvider
+
+# JSON
+import json
+
