5c97e70bd30c3212840cb7066495b460f14313d9
Murat Saglam <muratusaglam@gmail.com>
2022-10-30 00:17:38 +0100

    FIX DASHBOARD

--- web3ToolKit/models.py
+++ web3ToolKit/models.py
@@ -1,3 +1,18 @@
+from datetime import date
+from lib2to3.pgen2 import token
 from django.db import models
+from django.test import TransactionTestCase
 
+# Represents the data in your database
 # Create your models here.
+
+
+class wallet(models.Model):
+    # id = models.AutoField(primary_key=True)
+    address = models.CharField(max_length=36)
+    token = models.CharField(max_length=16)
+    transactionValue = models.DecimalField(max_digits=100, decimal_places=0)
+    date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.address
--- web3ToolKit/views.py
+++ web3ToolKit/views.py
@@ -1,12 +1,19 @@
 from json import load
+from black import out
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import wallet
 
 # request handler - function that takes request and returns response
 # Create your views here.
 
 
 def index(request):
+    myWallet = wallet.objects.all().values()  # get all the data from the database
+    output = ""
+    for x in myWallet:
+        output += x["address"]
+
     template = loader.get_template("index.html")
-    return HttpResponse(template.render())
+    return HttpResponse(output)
